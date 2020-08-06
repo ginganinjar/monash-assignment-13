77bb14cf6146483621eadd9690b13ac9775124aa
David Sparrius <david@mstone.com.au>
2020-08-06 23:02:20 +1000

    about to delete id

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,11 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burgers.js");
+var burger = require("../models/burgers.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
+    
     var burgerObj = {
       burger: data
     };
@@ -16,11 +17,16 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+
+// name: $("#ca").val().trim(),
+// status : "served",
+// action_date: Date.now()
+
+router.post("/api/burger", function(req, res) {
+  burger.create([
+    "name", "status", "action_date"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.status, req.body.action_date
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -32,7 +38,7 @@ router.put("/api/cats/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -47,7 +53,7 @@ router.put("/api/cats/:id", function(req, res) {
 router.delete("/api/cats/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,13 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+
+$(".burgerToEat").on("click", function() {
+    let daBurgerToEat = $(this).data("id"); //  this is the id to delete
+    console.log(daBurgerToEat);
+ 
+
+})
+
   $(".change-sleep").on("click", function(event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
@@ -24,19 +32,21 @@ $(function() {
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
-    var newCat = {
+   var bla = $("#ca").val().trim();
+    alert(bla);
+    var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      status : "served",
+      action_date: Date.now()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burger", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
