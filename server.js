348ce81bb095dd97023fb402791caa4e0c26dd85
David Sparrius <david@mstone.com.au>
2020-08-08 21:42:05 +1000

    fixed

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("/express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("/controllers/burgerController.js");
+var routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
